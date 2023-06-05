d5a7504f9930b02c64d3827fac6096c4c1eb2559
SaitovN <nuriksaitov03@gmailcom>
2023-06-05 16:21:49 +0600

    Create ArrayErrors.java

---
+++ ArrayErrors.java
@@ -0,0 +1,11 @@
+package academy.devonline.java.basic.section01_setup.section06_setup;
+
+public class ArrayErrors {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5};
+//        System.out.println(array[-1]);
+//        System.out.println(array[500]);
+        System.out.println(array[4]);
+        System.out.println(array.length);
+    }
+}
